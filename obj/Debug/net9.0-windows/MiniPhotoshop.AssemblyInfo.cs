ac8ca966a42bf4dd5e1d6520dc8e8eefa4224dc6
MYusronN <hawkinsdamien007@gmail.com>
2025-10-19 12:43:18 +0700

    menambahkan kode PINDAHKAN DATA KE ARRAY 3D

--- obj/Debug/net9.0-windows/MiniPhotoshop.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/MiniPhotoshop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniPhotoshop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23c79e46659d4c30518bc4b115092d415858327f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniPhotoshop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiniPhotoshop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
